63672db21fcc3e36d40ce4f1d2eecf7d5075bb71
Michael Nigh <mic.nigh@gmail.com>
2023-01-16 14:36:03 -0600

    Deploy

--- out/_next/static/-LtQnW-IVw5NUcm0p5p-X/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/75fc9c18-0cf132890bf6dedc.js","static/chunks/531-f42d9fc29ce13f00.js","static/chunks/pages/index-40d7b57bd7c4437c.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
