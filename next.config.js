c7e0b8c22e8fdf38bc1aada78b5e4ffc53b7b5cd
Michael Nigh <mic.nigh@gmail.com>
2023-01-16 11:06:24 -0600

    generate gh-pages compatible output

--- next.config.js
+++ next.config.js
@@ -13,6 +13,9 @@ const withMDX = require('@next/mdx')({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
   // Configure pageExtensions to include md and mdx
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   // Optionally, add any other Next.js config below
