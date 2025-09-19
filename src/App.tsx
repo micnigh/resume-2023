53fe8a56e7aafb568c181acf8b48a163656e41ce
Michael Nigh <mic.nigh@gmail.com>
2025-09-18 21:15:10 -0500

    remove loading states

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,28 @@
-import React, { useEffect, useState } from 'react'
-import denormalizeExperience from './lib/data/normalizr/denormalizr/experience'
-import { getExperiences } from './lib/data'
+import React, { useEffect } from "react";
+import denormalizeExperience from "./lib/data/normalizr/denormalizr/experience";
+import { data } from "./lib/data";
 
-import { Header } from './components/Header'
-import { Summary } from './components/Summary'
-import { Skills } from './components/Skills'
-import { Experience } from './components/Experience'
-import { Education } from './components/Education'
-import { generateTooltips } from './lib/util/tooltip'
+import { Header } from "./components/Header";
+import { Summary } from "./components/Summary";
+import { Skills } from "./components/Skills";
+import { Experience } from "./components/Experience";
+import { Education } from "./components/Education";
+import { generateTooltips } from "./lib/util/tooltip";
 
 const App: React.FC = () => {
-  const [data, setData] = useState<any>(null)
-  const [loadError, setLoadError] = useState<Error | null>(null)
-  const [isLoading, setIsLoading] = useState(true)
-
   useEffect(() => {
-    const loadData = async () => {
-      try {
-        setIsLoading(true)
-        const experiences = await getExperiences()
-        setData(experiences)
-        setLoadError(null)
-      } catch (error) {
-        setLoadError(error as Error)
-        console.error('Failed to load data:', error)
-      } finally {
-        setIsLoading(false)
-      }
-    }
+    generateTooltips();
+  }, []);
 
-    loadData()
-  }, [])
-
-  useEffect(() => {
-    if (data) {
-      generateTooltips()
-    }
-  }, [data])
-
-  if (loadError) {
-    return <div>Failed to load data</div>
-  }
-
-  if (isLoading || !data) {
-    return <div>Loading...</div>
-  }
-
-  const { entities, result } = data
-  const experiences = result.map(k => denormalizeExperience(k, entities))
-  const tags = entities.tags ? Object.keys(entities.tags).map(id => entities.tags[id]) : []
+  const { entities, result } = data;
+  const experiences = result.map((k) => denormalizeExperience(k, entities));
+  const tags = entities.tags
+    ? Object.keys(entities.tags).map((id) => entities.tags[id])
+    : [];
 
   return (
     <div className="font-special">
-      <main
-        className="max-w-4xl mx-auto p-4 pt-8 space-y-5 print:p-4"
-      >
+      <main className="max-w-4xl mx-auto p-4 pt-8 space-y-5 print:p-4">
         <Header />
         <Summary />
         <Skills tags={tags} />
@@ -63,7 +31,7 @@ const App: React.FC = () => {
         <Education />
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/lib/data/index.ts
+++ src/lib/data/index.ts
@@ -1,1 +1,5 @@
-export { getExperiences } from './experiences';
+import getExperiences from "./experiences";
+
+export { getExperiences } from "./experiences";
+
+export const data = await getExperiences();
