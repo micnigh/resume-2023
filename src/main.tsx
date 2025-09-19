47413dac8f1b30eb9eca4ed3c56f67f8ebcaffbd
Michael Nigh <mic.nigh@gmail.com>
2025-09-18 21:11:53 -0500

    oops - readd tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/globals.css'
+import './styles/tailwind.css'
 import 'tippy.js/dist/tippy.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
